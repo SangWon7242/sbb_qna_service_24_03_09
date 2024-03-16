1a85f80b660fc27232ac9296f4dae12fae89c3d3
SangWon7242 <gotkdqja@gmail.com>
2024-03-16 10:21:37 +0900

    action에서 자동으로 조립된 객체를 입력 받기

--- src/main/java/com/sbs/exam/sbb/HomeController.java
+++ src/main/java/com/sbs/exam/sbb/HomeController.java
@@ -386,6 +386,20 @@ public class HomeController {
 
     return "%d번 게시물을 삭제하였습니다.".formatted(id);
   }
+
+  @GetMapping("/addPersonOldWay")
+  @ResponseBody
+  public Person addPersonOldWay(int id, int age, String name) {
+    Person p = new Person(id, age, name);
+
+    return p;
+  }
+
+  @GetMapping("/addPerson")
+  @ResponseBody
+  public Person addPerson(Person p) {
+    return p;
+  }
 }
 
 class Animal {
@@ -459,4 +473,12 @@ class Article {
     this(++lastId, title, body);
   }
 
+}
+
+@AllArgsConstructor
+@Getter
+class Person {
+  private int id;
+  private int age;
+  private String name;
 }

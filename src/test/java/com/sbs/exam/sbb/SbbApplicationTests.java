be01a98d2ed62d9e66693be6c430dac53dc2c6ed
SangWon7242 <gotkdqja@gmail.com>
2024-03-23 10:22:38 +0900

    question 테이블을 깔끔하게 지워주는 테스트케이스 추가

--- src/test/java/com/sbs/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/sbs/exam/sbb/SbbApplicationTests.java
@@ -23,12 +23,29 @@ class SbbApplicationTests {
 	}
 
 	@Test
-	void testJpa() {
+	void testJpa0() {
 		Question q1 = new Question();
 		q1.setSubject("sbb가 무엇인가요?");
 		q1.setContent("sbb에 대해서 알고 싶습니다.");
 		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);  // 첫번째 질문 저장
+		questionRepository.save(q1);  // 첫번째 질문 저장
+
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q2);  // 두번째 질문 저장
+
+		questionRepository.truncate();
+	}
+
+	@Test
+	void testJpa1() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q1);  // 첫번째 질문 저장
 
 		System.out.println(q1.getId());
 
@@ -36,7 +53,7 @@ class SbbApplicationTests {
 		q2.setSubject("스프링부트 모델 질문입니다.");
 		q2.setContent("id는 자동으로 생성되나요?");
 		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);  // 두번째 질문 저장
+		questionRepository.save(q2);  // 두번째 질문 저장
 
 		// id는 최소 0번보다 크다.
 		assertThat(q1.getId()).isGreaterThan(0);

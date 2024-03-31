9f63481b6d4de953e93d03f7225f2820542a709d
SangWon7242 <gotkdqja@gmail.com>
2024-03-31 09:20:44 +0900

    질문 리스트를 표 형태로 보여주기

--- src/main/java/com/sbs/exam/sbb/QuestionController.java
+++ src/main/java/com/sbs/exam/sbb/QuestionController.java
@@ -1,20 +1,29 @@
 package com.sbs.exam.sbb;
 
+import com.sbs.exam.sbb.qustion.Question;
+import com.sbs.exam.sbb.qustion.QuestionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor // 생성자 주입
 public class QuestionController {
+
+  // @Autowired 필드 주입
+  private final QuestionRepository questionRepository;
 
   @GetMapping("/question/list")
   // 이 자리에 @ResponseBody가 없으면 resources/templates/question_list.html을 뷰로 삼는다.
-  public String list() {
-    return "question_list";
-  }
+  public String list(Model model) {
+    List<Question> questionList = questionRepository.findAll();
 
-  @GetMapping("/question/list2")
-  // 이 자리에 @ResponseBody가 없으면 resources/templates/question_list.html을 뷰로 삼는다.
-  public String list2() {
-    return "question_list2";
+    // 미리 실행된 question_list.html에서
+    // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
+    model.addAttribute("questionList", questionList);
+    return "question_list";
   }
 }

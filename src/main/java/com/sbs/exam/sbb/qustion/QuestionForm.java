070cbcc02a6c2d3c3239ac65258e55535d808fd8
SangWon7242 <gotkdqja@gmail.com>
2024-04-07 10:17:07 +0900

    질문 폼 체크 로직

---
+++ src/main/java/com/sbs/exam/sbb/qustion/QuestionForm.java
@@ -0,0 +1,11 @@
+package com.sbs.exam.sbb.qustion;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class QuestionForm {
+  private String subject;
+  private String content;
+}

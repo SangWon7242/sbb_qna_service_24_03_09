ae46c43c7699729366947a7aa279ec0e2349210b
SangWon7242 <gotkdqja@gmail.com>
2024-04-07 11:26:32 +0900

    답변폼에도 스프링 밸리데이션 도입

--- src/main/java/com/sbs/exam/sbb/qustion/QuestionController.java
+++ src/main/java/com/sbs/exam/sbb/qustion/QuestionController.java
@@ -1,11 +1,15 @@
 package com.sbs.exam.sbb.qustion;
 
+import com.sbs.exam.sbb.answer.AnswerForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -36,10 +40,11 @@ public class QuestionController {
   }
 
   @GetMapping("/detail/{id}")
-  public String detail(Model model, @PathVariable int id) {
+  public String detail(Model model, @PathVariable int id, AnswerForm answerForm) {
     Question question = questionService.getQuestion(id);
 
     model.addAttribute("question", question);
+    // model.addAttribute("answerForm", new AnswerForm(""));
 
     return "question_detail";
   }

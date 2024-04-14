5c14f2c856b05014090ab4714b06f44f9759ee5a
SangWon7242 <gotkdqja@gmail.com>
2024-04-14 10:59:59 +0900

    회원가입 폼, 처리

--- src/main/java/com/sbs/exam/sbb/user/UserService.java
+++ src/main/java/com/sbs/exam/sbb/user/UserService.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
+@RequiredArgsConstructor // 생성자 주입
 @Service
 public class UserService {
 

419bf2374211579b8543976aded47cf00517a0cc
SangWon7242 <gotkdqja@gmail.com>
2024-04-20 09:36:34 +0900

    시큐리티를 통한 로그인 폼 처리

--- src/main/java/com/sbs/exam/sbb/user/UserRepository.java
+++ src/main/java/com/sbs/exam/sbb/user/UserRepository.java
@@ -2,6 +2,10 @@ package com.sbs.exam.sbb.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
   boolean existsByUsername(String username);
+
+  Optional<SiteUser> findByusername(String username);
 }

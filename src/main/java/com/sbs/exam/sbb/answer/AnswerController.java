abe3b535cf09c2814bfab84f3ea3988edfc02128
SangWon7242 <gotkdqja@gmail.com>
2024-04-27 16:39:38 +0900

    질문 수정 폼

--- src/main/java/com/sbs/exam/sbb/answer/AnswerController.java
+++ src/main/java/com/sbs/exam/sbb/answer/AnswerController.java
@@ -1,18 +1,22 @@
 package com.sbs.exam.sbb.answer;
 
+import com.sbs.exam.sbb.DataNotFoundException;
 import com.sbs.exam.sbb.qustion.Question;
 import com.sbs.exam.sbb.qustion.QuestionService;
 import com.sbs.exam.sbb.user.SiteUser;
 import com.sbs.exam.sbb.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -43,5 +47,37 @@ public class AnswerController {
     // 답변 등록 끝
 
     return "redirect:/question/detail/%d".formatted(id);
+  }
+
+  @PreAuthorize("isAuthenticated()")
+  @GetMapping("/modify/{id}")
+  public String answerModify(AnswerForm answerForm, @PathVariable("id") Long id, Principal principal) {
+    Answer answer = answerService.getAnswer(id);
+
+    if(answer == null) {
+      throw new DataNotFoundException("데이터가 없습니다.");
+    }
+
+    if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+    }
+    answerForm.setContent(answer.getContent());
+    return "answer_form";
+  }
+
+  @PreAuthorize("isAuthenticated()")
+  @PostMapping("/modify/{id}")
+  public String answerModify(@Valid AnswerForm answerForm, BindingResult bindingResult,
+                             @PathVariable("id") Long id, Principal principal) {
+    if (bindingResult.hasErrors()) {
+      return "answer_form";
+    }
+    Answer answer = this.answerService.getAnswer(id);
+    if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+    }
+
+    answerService.modify(answer, answerForm.getContent());
+    return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
   }
 }
--- src/main/java/com/sbs/exam/sbb/answer/AnswerService.java
+++ src/main/java/com/sbs/exam/sbb/answer/AnswerService.java
@@ -1,5 +1,6 @@
 package com.sbs.exam.sbb.answer;
 
+import com.sbs.exam.sbb.DataNotFoundException;
 import com.sbs.exam.sbb.qustion.Question;
 import com.sbs.exam.sbb.user.SiteUser;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +22,16 @@ public class AnswerService {
 
     question.addAnswer(answer);
 
+    answerRepository.save(answer);
+  }
+
+  public Answer getAnswer(Long id) {
+    return answerRepository.findById(id).orElseThrow(() -> new DataNotFoundException("answer not found"));
+  }
+
+  public void modify(Answer answer, String content) {
+    answer.setContent(content);
+    answer.setModifyDate(LocalDateTime.now());
     answerRepository.save(answer);
   }
 }
